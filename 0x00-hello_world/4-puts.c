42fe2add7f727c6214620f228ebe0416d72a0fd0
Mbatha-Marvin <github@mbathamarvin.com>
2022-06-27 01:13:29 -0700

    Corrected an error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle"; 
-	
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
-	
+
 	return (0);
 }
