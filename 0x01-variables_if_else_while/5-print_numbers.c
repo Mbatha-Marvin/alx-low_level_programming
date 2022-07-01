c66c71f41cd850478699b695536400d99c6f9587
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-01 04:50:43 -0700

    prints out all single digit base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints out decimal numbers form 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <10)
+	{
+		printf(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
