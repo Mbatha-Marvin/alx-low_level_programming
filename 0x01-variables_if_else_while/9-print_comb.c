7f8202925820314ed0464492e7aa45b7cde7edcf
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-01 06:31:58 -0700

    combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints formated numbers
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+	int c;
+
+	while (num < 10)
+	{
+		c = num + '0';
+		putchar(c);
+		if (num !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
