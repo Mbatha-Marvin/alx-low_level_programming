d13cf82e6f163d1ea2b302b5ed4e8c773fe0ff25
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-05 07:00:23 -0700

    Using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using the _putchar instead of built
+ * function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str1[] = "_putchar\n";
+	int length = strlen(str1);
+	int i;
+
+	for(i = 0; i < length; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
