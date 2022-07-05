e684a4f8e3149f6eeff58fa0feae65bc944bba75
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-05 10:44:15 -0700

    function adds to numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @n: first number
+ * @m: second number
+ *
+ * Description: adds two numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int n, int m)
+{
+	int res = n + m;
+
+	return (res);
+}
