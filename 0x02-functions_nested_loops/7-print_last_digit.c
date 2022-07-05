cb80ba83c4e5fd8173a64a759679d6f425d9b749
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-05 10:00:25 -0700

    Prints out the last digit of the input

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: integer input to be evaluated
+ *
+ * Description: Prints out the last digit of the input
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	return (last);
+}
