2ede9da1b6c53f6d4ca5cfee01e1aee05b54f69b
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-05 08:24:37 -0700

    print the alphabet in lowercase using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints out the alphabet
+ * in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
