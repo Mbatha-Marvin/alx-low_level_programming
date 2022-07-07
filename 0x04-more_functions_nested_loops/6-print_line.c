725c9fb810411926ea3452ca369677f9a5aac7d4
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-07 10:47:56 -0700

    Prints line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: length of line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			break;
+		_putchar('_');
+	}
+
+	_putchar('\n')
+}
