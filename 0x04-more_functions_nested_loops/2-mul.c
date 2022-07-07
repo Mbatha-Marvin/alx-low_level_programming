93bf0b4049c54a6842bf7594456f8fc683c68100
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-07 05:44:22 -0700

    returns product of inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiples two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product on (Success)
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
