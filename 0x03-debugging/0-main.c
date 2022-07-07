5d6a7c10fd6bcadcf708b0a71a21c24bd2725930
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-06 21:01:12 -0700

    test if function gives the correct output

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: checks if the function prints right output
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
