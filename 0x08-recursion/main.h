0f995f5ea4e89b95c866e8f78acbca7f9004f7db
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-20 04:36:05 -0700

    _puts_recursion - prints string recursively

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,32 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _puts_recursion - uses recursion to print strings
+ * @s: string to be printed recursively
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion - prints a string recursively in reverse
+ * @s: string to be printed in reverse
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s);
+
+/**
+ * _putchar - prints a character
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned
+ */
+
+int _putchar(char c);
+
+#endif
