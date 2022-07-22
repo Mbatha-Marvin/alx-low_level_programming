756c8e33bf907266bde133836fe6fd1440457565
Mbatha-Marvin <89975643+Mbatha-Marvin@users.noreply.github.com>
2022-07-22 23:20:19 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,55 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @argc: number of arguments
- * @argv: array storing the arguments
- *
- * Return: 0 an success else 1
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool is_num(char *argvv)
+{
+	int j = 0;
+
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
  */
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i = 1;
+	int sum = 0;
 
+	/* validate input */
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
+		return (0);
 	}
-	for (i = 1; i < argc; i++)
+
+	/* check all arguments to add numbers */
+	while (i < argc)
 	{
-		if (isdigit(atoi(argv[i])) == 0)
-		{
+		if (is_num(argv[i]))
 			sum += atoi(argv[i]);
-		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+		i++;
 	}
-
-	printf("%s\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
