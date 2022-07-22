fcf4aaff65fa12dbf1584167ca0a1a9722570dc4
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-22 11:21:21 -0700

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array containing command line arguments
+ *
+ * Return: Always (0) success
+ */
+
+int main (int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d", (argc - 1));
+
+	return (0);
+}
