60d89bad6fe70a112fd29c55a1dd56e31c6520ab
Mbatha-Marvin <github@mbathamarvin.com>
2022-07-22 10:58:05 -0700

    a program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always (0) success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Learning about main arguments argc and argv[]
