1239e7217804ef275c85e54ae5948e84fc4b6bcc
Mbatha-Marvin <mbathamarvin597@gmail.com>
2022-08-23 01:14:40 +0300

    File IO Advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,7 +1,11 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <elf.h>
+
 /**
  * print_addr - prints address
  * @ptr: magic.
