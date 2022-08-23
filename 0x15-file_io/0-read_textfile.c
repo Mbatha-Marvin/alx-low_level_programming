f64f7c5d8dd7cebd59982deac85904f80b1587ce
Mbatha-Marvin <mbathamarvin597@gmail.com>
2022-08-23 17:43:25 +0300

    fixing bugs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		close(fd);
 		return (0);
-	}	
+	}
 	lenr = read(fd, buffer, letters);
 	close(fd);
 	if (lenr == -1)
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);	
+	free(buffer);
 	if (lenr != lenw)
 		return (0);
 	return (lenw);
